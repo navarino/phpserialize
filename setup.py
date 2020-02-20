a3edb1392f930ee44acbc3806d39dba3cce907c8
Vasilis Gerakaris <vasilis.gerakaris@navarino.gr>
2020-02-20 13:12:01 +0200

    Revert url to mitsuhiko's repo

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     author='Armin Ronacher',
     author_email='armin.ronacher@active-4.com',
     version='1.5',
-    url='https://github.com/nathanwalsh/phpserialize',
+    url='https://github.com/mitsuhiko/phpserialize',
     py_modules=['phpserialize'],
     description='a port of the serialize and unserialize '
                 'functions of php to python.',
